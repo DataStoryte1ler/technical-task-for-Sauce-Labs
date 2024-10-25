4ba951466847f0de3863457be4771449eb905153
Yaroslav Yushchenko <65304153+DataStoryte1ler@users.noreply.github.com>
2024-10-26 00:42:09 +0200

    Add files via upload

---
+++ brokenSauce.js
@@ -0,0 +1,87 @@
+const {Builder, By, Key, until} = require('selenium-webdriver')
+const utils = require('./utils')
+
+const SAUCE_USERNAME = process.env.SAUCE_USERNAME;
+const SAUCE_ACCESS_KEY = process.env.SAUCE_ACCESS_KEY;
+//const ONDEMAND_URL = `https://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.us-west-1.saucelabs.com:443/wd/hub`;
+// NOTE: Use the URL below if using our EU datacenter (e.g. logged in to app.eu-central-1.saucelabs.com)
+const ONDEMAND_URL = `https://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.eu-central-1.saucelabs.com:443/wd/hub`;
+
+
+/**
+* Run this test before working on the problem.
+* When you view the results on your dashboard, you'll see that the test "Failed".
+* Your job is to figure out why the test failed and make the changes necessary to make the test pass.
+* Once you get the test working, update the code so that when the test runs, it can reach the Sauce Labs homepage,
+* hover over 'Resources' and then clicks the 'Documentation' link
+*/
+
+describe('Broken Sauce', function () {
+    it('should go to Google and click Sauce', async function () {
+
+        try {
+            let driver = await new Builder().withCapabilities(utils.brokenCapabilities)
+                    .usingServer(ONDEMAND_URL).build();
+
+        /*            
+        await driver.get("https://www.google.com");
+        // If you see a German or English GDPR modal on google.com you 
+        // will have to code around that or use the us-west-1 datacenter.
+        // You can investigate the modal elements using a Live Test(https://app.saucelabs.com/live/web-testing)
+        
+        // I wasn't able to pypass GDPR, despite trying a bunch of different methods
+        let AcceptButton = await driver.wait(until.elementLocated(By.xpath("//*[contains(text(), 'Alle akzeptieren')]")), 10000);
+        await driver.wait(until.elementIsVisible(AcceptButton), 10000);
+        await AcceptButton.click();
+
+
+        let search = await driver.findElement(By.name("q"));
+        await search.sendKeys("Sauce Labs");
+        
+        let button = await driver.findElement(By.name("btnK"))
+        await button.click()
+
+        let page = await driver.wait(until.elementLocated(By.partialLinkText("Sauce")), 20000);
+        await button.click()
+        */
+
+        // Navigate to Sauce Labs website and find 'Recources' menu
+        await driver.get("https://saucelabs.com");
+        let resources = await driver.wait(until.elementLocated(By.xpath("//*[text()='Resources']")), 10000);
+        await driver.wait(until.elementIsVisible(resources), 10000);
+        
+        // Perform hover action over Resources menu
+        let actionRes = driver.actions({ async: true });
+        await actionRes.move({ origin: resources }).perform();
+
+        // There's no 'Documentation' link in the 'Recources' dropdown menu
+        let developers = await driver.wait(until.elementLocated(By.xpath("//*[text()='Developers']")), 10000);
+        await driver.wait(until.elementIsVisible(developers), 10000);
+        
+        // Therefore, navigating to 'Developers' dropdown menu
+        let actionDev = driver.actions({ async: true });
+        await actionDev.move({ origin: developers }).perform();
+        
+        // Locate and click the 'Documentation' link in the dropdown menu
+        let documentationLink = await driver.wait(until.elementLocated(By.xpath("//a[@href='https://docs.saucelabs.com/']")), 10000);
+        await driver.wait(until.elementIsVisible(documentationLink), 10000);
+        await documentationLink.click();
+
+        await driver.wait(until.urlIs("https://docs.saucelabs.com/"), 10000);
+
+        
+
+        await driver.quit();
+        } catch (err) {
+            // hack to make this pass for Gitlab CI
+            // candidates can ignore this
+            if (process.env.GITLAB_CI === 'true') {
+                console.warn("Gitlab run detected.");
+                console.warn("Skipping error in brokenSauce.js");
+            } else {
+                throw err;
+            }
+        }
+
+    });
+});
---
+++ workingSauce.js
@@ -0,0 +1,76 @@
+const {Builder, By, Key, until} = require('selenium-webdriver')
+const SauceLabs = require('saucelabs').default;
+const assert = require('assert');
+const utils = require('./utils')
+
+const SAUCE_USERNAME = process.env.SAUCE_USERNAME;
+const SAUCE_ACCESS_KEY = process.env.SAUCE_ACCESS_KEY;
+//const ONDEMAND_URL = `https://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com:443/wd/hub`;
+// NOTE: Use the URL below if using our EU datacenter (e.g. logged in to app.eu-central-1.saucelabs.com)
+const ONDEMAND_URL = `https://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.eu-central-1.saucelabs.com:443/wd/hub`;
+
+/**
+* Task I: Update the test code so when it runs, the test clicks the "I am a link" link.
+*
+* Task II - Comment out the code from Task I. Update the test code so when it runs, 
+* the test is able to write "Sauce" in the text box that currently says "I has no focus".
+*
+* Task III - Update the test code so when it runs, it adds an email to the email field, 
+* adds text to the comments field, and clicks the "Send" button.
+* Note that email will not actually be sent!
+*
+* Task IV - Add a capability that adds a tag to each test that is run.
+* See this page for instructions: https://docs.saucelabs.com/dev/test-configuration-options/
+* 
+* Task V: Set the status of the test so it shows as "passed" instead of "complete".
+* We've included the node-saucelabs package already. For more info see:
+* https://github.com/saucelabs/node-saucelabs
+*/
+
+describe('Working Sauce', function () {
+    it('should go to Google and click Sauce', async function () {
+        let driver = await new Builder().withCapabilities(utils.workingCapabilities)
+                    .usingServer(ONDEMAND_URL).build();
+
+
+    /**
+     * Goes to Sauce Lab's guinea-pig page and verifies the title
+     */
+
+    await driver.get("https://saucelabs.com/test/guinea-pig");
+    await assert.strictEqual("I am a page title - Sauce Labs", await driver.getTitle());
+
+    // Task I
+    /*
+    const linkId = 'i am a link';
+    const linkElement = await driver.wait(until.elementLocated(By.id(linkId)), 20000);
+    await linkElement.click();
+    */
+
+    // Task II
+    const textBoxId = 'i_am_a_textbox';
+    const textBoxElement = await driver.wait(until.elementLocated(By.id(textBoxId)), 20000);
+    await textBoxElement.clear();
+    await textBoxElement.sendKeys('Sauce');
+
+    // Task III
+    // Adds an email to the email field
+    const emailTextBoxId = 'fbemail';
+    const emailElement = await driver.wait(until.elementLocated(By.id(emailTextBoxId)), 20000);
+    await emailElement.sendKeys('yar.mail134@gmail.com');
+    
+    // Adds text to the comments field
+    const commentsTextBoxId = 'comments'
+    const commentsElement = await driver.wait(until.elementLocated(By.id(commentsTextBoxId)), 20000);
+    await commentsElement.sendKeys('I like this task!');
+    
+    // Clicks the 'Send' button
+    const sendButtonClass = 'jumpButton';
+    const sendButtonElement = await driver.wait(until.elementLocated(By.className(sendButtonClass)), 20000);
+    await sendButtonElement.click();
+
+    // Task V: I wasn't able to adjust the status, CLI Tool didn't work for me
+
+    await driver.quit();
+    });
+});
